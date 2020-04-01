53b4ccd74c124a058bf2581dd318abdb37372cc4
yann.dufresne@etu.umontpellier.fr <yann.dufresne@etu.umontpellier.fr>
2020-04-01 16:00:53 +0200

    creation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { jqxTreeGridComponent } from 'jqwidgets-ng/jqxtreegrid';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,52 +8,5 @@ import { jqxTreeGridComponent } from 'jqwidgets-ng/jqxtreegrid';
 
 export class AppComponent {
   title = 'client';
-
-    @ViewChild('treeGridReference', {static: false}) treeGrid: jqxTreeGridComponent;
-
-    source: any =
-    {
-        dataType: "json",
-        url: "./assets/ontologieHotellerie.json",
-        dataFields: [
-            { name: "id", type: "number" },
-            { name: "part", type: "string" },
-            { name: "polarity", type: "number" },
-            { name: "synonyms", type: "string" },
-            { name: "subpart", type: "array" }
-        ],
-        root: "root",
-        id: "id",
-        hierarchy:
-        {
-        	root: "subpart"
-        }
-    }
-
-    dataAdapter: any = new jqx.dataAdapter(this.source);
-
-    columns: any[] =
-    [
-        { text: "part", dataField: "part", minWidth: 200, width: 200 },
-        { text: "polarity", dataField: "polarity", width: 100,
-
-            cellsRenderer: function (row, column, value, rowData) {
-                if (value <0) {
-                    return '<span style="color: #D00000; font-weight: bold;">' + value + '</span>';
-                }
-                if (value >0) {
-                    return '<span style="color: #00D000; font-weight: bold;">' + value + '</span>';
-                }
-                return value;
-            }
-
-        },
-        { text: "synonyms", dataField: "synonyms", width: 200 }
-    ];
-
-    ready: any = () => {
-//            this.treeGrid.expandAll();
-            this.treeGrid.collapseAll();
-    };
 
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { PolariteServiceService } from './_services/polarite-service.service';
 import { SentenceTagComponent } from './sentence-tag/sentence-tag.component';
 import { jqxTreeGridComponent } from 'jqwidgets-ng/jqxtreegrid';
+import { OntologyComponent } from './ontology/ontology.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     SentenceTagComponent,
-    jqxTreeGridComponent
+    jqxTreeGridComponent,
+    OntologyComponent
   ],
   imports: [
     BrowserModule,
